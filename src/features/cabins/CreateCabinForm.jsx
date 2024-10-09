32087b7a8c6c507ea8a0e6f5dffb2218d482a19d
OMPRATIK <routprateek4@gmail.com>
2024-10-09 14:16:28 +0530

    complete modal

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -36,6 +36,7 @@ function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
         {
           onSuccess: () => {
             reset();
+            onCloseModal?.();
           },
         }
       );
